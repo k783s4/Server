45ba84f6ed1c9988989fe55c42c4264abbaf1d46
Leonardo Puzzuoli <chipleo.codes@gmail.com>
2018-01-04 19:43:54 +0100

    started writing instructions

--- checkaccessrights.js
+++ checkaccessrights.js
@@ -1,29 +1,26 @@
 //check for the rights of the request
 require("./addons");
 module.exports.allow = function allow(token,request,callback){
-  console.log("dwad");
   if(token === "Server"){
-    console.log("d");
     callback(true);
   }
-  console.log("1");
   if(token === "SU" && !request.contains("server_only")){
-    console.log("n");
     callback(true);
   }
-  console.log("2");
   if(request.contains("server_only") || request.contains("private") || request.contains("public")){
     if(request.contains("server_only")){
-      console.log("c");
       callback(false);
     }
-    console.log("3");
     if(request.contains("private") && ((token !="Server" || token != "SU"))){
-      console.log("b");
       callback(false);
     }
-    console.log("4");
+    if(!checkToken(token)){
+      callback(false)
+    }
   }
-    console.log("callbackstandatrd");
     callback(true);
+}
+function checkToken(token){
+  //this function is customizable. it should compare a given token with valid tokens and return true or false
+  return true;
 }
